cc340b81782d388a9e47af39c147865fad217038
Lane Vance <lanevance92@gmail.com>
2023-04-15 16:14:05 -0600

    Server updated

--- server/controller.js
+++ server/controller.js
@@ -6,23 +6,28 @@ module.exports = {
     getPassport: (req, res) => res.status(200).send(passport),
     addToPassport: (req, res) => {
         let id = req.params.id
-        let index = parks.findIndex((park) => park.id === +id)
-        passport.push(parks[index])
-        res.status(200).send(passport)
+        let park = parks.find((p) => p.id === +id)
+        passport.push(park)
+        res.status(200).send()
     },
     deleteFromPassport: (req, res) => {
         let id = req.params.id
         let index = passport.findIndex((park) => park.id === +id)
+        if(index < 0){
+            res.status(404).send()
+            return
+        }
         passport.splice(index, 1)
-        res.status(200).send(passport)
-    },
-    addNotes: (req, res) => {
-
-        res.status(200).send(something)
+        res.status(200).send()
     },
     updateNotes: (req, res) => {
-        // let index = req.params.id - 1
-
-        res.status(200).send(something)
+        let id = req.params.id
+        let park = passport.find((p) => p.id === +id)
+        if(park === undefined){
+            res.status(404).send()
+            return
+        }
+        park.notes = req.body.notes
+        res.status(200).send()
     }
 }
--- server/index.js
+++ server/index.js
@@ -8,13 +8,16 @@ const app = express()
 //Middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static(`${__dirname}/../client`))
 
 //Endpoints
-const {getParks, addToPassport, deleteFromPassport} = require('./controller')
+const {getParks, getPassport, addToPassport, deleteFromPassport, updateNotes} = require('./controller')
 
 app.get('/parks', getParks)
+app.get('/passport', getPassport)
 app.post('/passport/:id', addToPassport)
 app.delete('/passport/:id', deleteFromPassport)
+app.put('/passport/:id', updateNotes)
 
 //Start server with app.listen
 app.listen(8008, () => console.log('The server is up and running, NICE!'))
